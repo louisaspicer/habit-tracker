a9f4f897d1c6cc8bd99f87f4ba62c3eaad94d899
Louisa Spicer <louisaspicer@gmail.com>
2020-06-11 13:43:52 +0100

    Add styled components

---
+++ styled/theme.ts
@@ -0,0 +1,45 @@
+export const spacing = {
+  xs: 4,
+  s: 8,
+  m: 16,
+  l: 24,
+  xl: 32,
+  xxl: 48,
+};
+
+export const typography = {
+  size: {
+    text: {
+      l: 20,
+      m: 16,
+      s: 14,
+      xs: 12,
+    },
+    heading: {
+      xl: 32,
+      l: 24,
+      m: 20,
+      s: 16,
+    },
+  },
+  lineheight: {
+    body: {
+      l: 28,
+      m: 24,
+      s: 20,
+      xs: 18,
+    },
+    heading: {
+      xl: 48,
+      l: 36,
+      m: 28,
+      s: 24,
+    },
+  },
+};
+
+export const colors = {
+  black: "#000000",
+  white: "#FFFFFF",
+  gray: "#323E4A",
+};
