fa19884731eacf95c38e3bcf695838d779319d6f
Louisa Spicer <louisaspicer@gmail.com>
2020-06-11 12:28:11 +0100

    Add bottom nav

--- App.tsx
+++ App.tsx
@@ -2,6 +2,13 @@ import React from "react";
 import { StyleSheet, Text, View } from "react-native";
 import { NavigationContainer } from "@react-navigation/native";
 import { createStackNavigator } from "@react-navigation/stack";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import Ionicons from "@expo/vector-icons/Ionicons";
+
+type BottomTabParams = {
+  Home: undefined;
+  Journal: undefined;
+};
 
 function HomeScreen() {
   return (
@@ -11,14 +18,48 @@ function HomeScreen() {
   );
 }
 
-const Stack = createStackNavigator();
+function JournalScreen() {
+  return (
+    <View style={styles.container}>
+      <Text>Journal</Text>
+    </View>
+  );
+}
+
+const Tab = createBottomTabNavigator<BottomTabParams>();
 
 export default function App() {
   return (
     <NavigationContainer>
-      <Stack.Navigator>
-        <Stack.Screen name="Home" component={HomeScreen} />
-      </Stack.Navigator>
+      <Tab.Navigator
+        screenOptions={({ route }) => ({
+          tabBarIcon: ({ focused, color, size }) => {
+            let iconName;
+            // TODO: replace icons
+            if (route.name === "Home") {
+              iconName = focused
+                ? "ios-information-circle"
+                : "ios-information-circle-outline";
+            } else if (route.name === "Journal") {
+              iconName = focused ? "ios-list-box" : "ios-list";
+            }
+            return <Ionicons name={iconName} size={size} color={color} />;
+          },
+        })}
+        tabBarOptions={{
+          activeTintColor: "tomato",
+          inactiveTintColor: "gray",
+        }}
+      >
+        <Tab.Screen
+          name="Home"
+          options={{
+            title: "Home",
+          }}
+          component={HomeScreen}
+        />
+        <Tab.Screen name="Journal" component={JournalScreen} />
+      </Tab.Navigator>
     </NavigationContainer>
   );
 }
